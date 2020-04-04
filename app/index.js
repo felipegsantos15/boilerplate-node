b380b46261cdec3052a5a00d1635109c0bfc23ab
felipe.santos <felipe.santos@pontaltelecom.com.br>
2020-04-04 14:24:37 -0300

    correção de bugs

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const restify = require('restify');
 const server = restify.createServer();
 const router = require('./router');
 
-server.router(router);
+router.router(server);
 
 server.listen(process.env.PORT, () => {
     console.log(`server listen on port ${process.env.PORT}`);
--- app/router.js
+++ app/router.js
@@ -1,7 +1,10 @@
 module.exports = {
-    router: () => {
+    router: (server) => {
         server.get('/ping', (req, res, next) => {
             res.send(200, {message: 'ping'});
+        });
+        server.get('/', (req, res, next) => {
+            res.send(200, {message: 'home'});
         });
     }
 }
